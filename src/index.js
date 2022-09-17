f51003c7c0de6da0e3876f034227df4ac6c0d996
EliasVasques <eliasvasques85@gmail.com>
2022-09-17 20:43:55 -0300

    adição: adciona blog  ao clicar em salvar na tela de addBlog

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const { conectarComMongo, pegarConexaoMongo } =require('./db')
 
 const app = express();
 
+// receber dados via post
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 // conectar com o banco de dados
 let db;
 conectarComMongo( ( erro ) => {
@@ -11,8 +16,6 @@ conectarComMongo( ( erro ) => {
         db = pegarConexaoMongo();
     }
 })
-
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/web/index.html')
@@ -25,5 +28,24 @@ app.get('/blogs', (req, res) => {
         .forEach(blog => blogs.push(blog))
         .then(() => {
             res.status(200).json(blogs)
+        })
+        .catch((erro) => {
+            res.status(500).json({ erro: 'Erro na busca dos blogs'})
+        })
+})
+
+app.get('/blogs/addBlog', (req, res) => {
+    res.sendFile(__dirname + '/web/addBlog.html')
+})
+
+app.post('/blogs/addBlog', (req, res) => {
+    const blog = req.body
+    db.collection('blogs')
+        .insertOne(blog)
+        .then(() => {
+            res.status(200).sendFile(__dirname + '/web/addBlog.html');
+        })
+        .catch((erro) => {
+            res.status(500).sendFile(__dirname + '/web/addBlog.html');
         })
 })
